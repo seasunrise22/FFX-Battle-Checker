3b220983c9738a405933121ce747ac0719aaf141
seasunrise22 <seasunrise22@gmail.com>
2020-09-23 15:47:16 +0900

    for문으로 텍스트뷰 동적 생성 개발

--- app/src/main/java/com/example/ffx_battle_checker/BattleActivity.java
+++ app/src/main/java/com/example/ffx_battle_checker/BattleActivity.java
@@ -21,15 +21,13 @@ public class BattleActivity extends AppCompatActivity {
         int count = intent.getIntExtra("count", 0);
         String[] name = intent.getStringArrayExtra("name");
 
-        TextView textView1 = new TextView(this);
-        textView1.setText(name[0]);
-        TextView textView2 = new TextView(this);
-        textView2.setText(name[1]);
-
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 0.1f);
-        textView1.setLayoutParams(params);
-        textView2.setLayoutParams(params);
-        dynamicLayout.addView(textView1);
-        dynamicLayout.addView(textView2);
+
+        for(int i=0; i<count; i++) {
+            TextView entity = new TextView(this);
+            entity.setText(name[i]);
+            entity.setLayoutParams(params);
+            dynamicLayout.addView(entity);
+        }
     }
 }
